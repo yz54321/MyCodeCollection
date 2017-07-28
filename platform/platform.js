fbce7c60df108921ba4c9973db9089342cedc5f3
yz <yangzhong@dcampus.com>
2017-07-28 10:53:19 +0800

    uploadManager&platform

--- platform/platform.js
+++ platform/platform.js
@@ -1,30 +1,41 @@
 
 
 
- {
-angular.module("utilsModule")
-    .factory("utils.platform",  platform);
-    platform.$inject = ['$window'];
-    function platform($window) {
-    	let collect={
-    		'isIOS':isIOS,
-    		'isAndroid':isAndroid,
-            'isWechat':isWechat
-    	}
-    	let ua=$window.navigator.userAgent;
-        console.log(ua);
-    	function isIOS(){
-    		return /iPhone|iPad|iPod/.test(ua);
-    	}
+{
+	angular.module("utilsModule")
+		.factory("utils.platform",  platform);
+	platform.$inject = ['$window'];
+	function platform($window) {
+		let collect={
+			'isAndroid':isAndroid,
+			'isIPhone':isIPhone,
+			'isIPad':isIPad,
+			'isIOS':isIOS,
+			'isMobile':isMobile,
+			'isWechat':isWechat
+		}
+		let ua=$window.navigator.userAgent.toLowerCase();;
+		// console.log(ua);
+		function isIPhone(){
+			return /iphone|ipod/.test(ua);
+		}
+		function isIPad(){
+			return /ipad/.test(ua);
+		}
+		function isIOS(){
+			return /iphone|ipad|ipod/.test(ua);
+		}
 
-    	function isAndroid(){
-    		return ua.indexOf('Android') > 0;
-    	}
+		function isAndroid(){
+			return ua.indexOf('android') > 0;
+		}
+		function isMobile(){
+			return isIPhone()||isAndroid();
+		}
+		function isWechat(){
+			return ua.match(/MicroMessenger/i)=="micromessenger";
+		}
 
-        function isWechat(){
-            return ua.toLowerCase().match(/MicroMessenger/i)=="micromessenger";
-        }
-
-    	return collect;
-    }
+		return collect;
+	}
 }
